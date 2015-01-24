f693c16a2c6f8c7d33daa1256db131eb548441fb
bernout0 <bernie.ngojo@gmail.com>
2015-01-25 04:07:38 +0800

    instructions

---
+++ ggjdemo/Assets/2 Scripts/showInstruction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class showInstruction : MonoBehaviour {
+	public Transform player;
+	public Vector3 desiredPosition;
+	public int xLimit;
+	// Use this for initialization
+	private bool shown = false;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.x >= xLimit && shown == false) {
+			transform.position = desiredPosition;
+			shown = true;
+				} else {
+
+				}
+	}
+
+
+}
