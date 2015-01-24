291d0fd503a76aa146dbc904ad69de18a1736180
bernout0 <bernie.ngojo@gmail.com>
2015-01-24 13:04:14 +0800

    Shader change + Tuts

---
+++ ggjdemo/Assets/2 Scripts/BackToCheckpoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToCheckpoint : MonoBehaviour {
+
+	public Vector3 lastGoodPosition = new Vector3( 0,0,0);
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Block")
+		{
+			lastGoodPosition = transform.position;
+		}
+		else if (collision.gameObject.tag == "Respawn")
+		{
+			Debug.Log ("asdD");
+			transform.position = lastGoodPosition;
+		}
+	}
+}
