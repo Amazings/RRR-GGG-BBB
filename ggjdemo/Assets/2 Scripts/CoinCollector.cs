cdb2580fe352fb11c8285f1984f5727badff665a
Chester Lumibao <chesterr400@gmail.com>
2015-01-24 20:28:56 +0800

    Added collectibles at level 1-3

---
+++ ggjdemo/Assets/2 Scripts/CoinCollector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinCollector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision obj){
+
+		if (obj.gameObject.tag == "Collect") {
+			Destroy (obj.gameObject);
+			Debug.Log ("Coin Collected");
+		}
+
+	
+	
+	}
+}
